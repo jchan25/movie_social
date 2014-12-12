6799858ce13bbadd55c1ba81f474389c90b146c5
Jonathan Chan <jchan25@gmail.com>
2014-12-11 22:31:30 -0600

    sql update

--- app/models/user_favorited_movie.rb
+++ app/models/user_favorited_movie.rb
@@ -2,8 +2,8 @@ class UserFavoritedMovie < ActiveRecord::Base
  belongs_to(:user, { :class_name => "User", :foreign_key =>"user_ID"})
  belongs_to(:movie, { :class_name => "Movie", :foreign_key =>"movie_ID"})
 
- validates :movie_ID, :presence => true
- validates :user_ID, :presence => true
+ validates :movie, :presence => true
+ validates :user, :presence => true
 
  validates_uniqueness_of :user_ID, scope: :movie_ID
  #validates :user_ID :uniqueness => true
